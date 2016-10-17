9e4db2784b6d21820dfada9f9d79fa1351e86c6d
Paula Lee <pdlee877@gmail.com>
2016-10-16 18:39:43 -0700

    nearby restaurants displays correctly

--- app/helpers/map_helper.rb
+++
@@ -1,6 +0,0 @@
-helpers do
-	def latitude(location)
-		destination = GooglePlacesApiAdapter.find_location(location)
-		p destination['results'][0]['geometry']['location']['lat']
-	end
-end
--- app/models/google_places_api_adapter.rb
+++ app/models/google_places_api_adapter.rb
@@ -6,15 +6,7 @@ class GooglePlacesApiAdapter
 		url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{latitude},#{longitude}&radius=500&type=restaurant&key=#{mykey}"
 		format :json
 		response = HTTParty.get(url)
-		p response
-		# p '*' * 50
-
-		# pictures = body['photos']
-		# name = body['name']
-		# icon = body['icon']
-		# hours = body['opening_hours']
-		# priceing = body['price_level']
-		# rating = body['rating']
+		body = response.parsed_response
 	end
 
 	# make a search box within the map to input the output of the activity
@@ -23,7 +15,6 @@ class GooglePlacesApiAdapter
 		url = "https://maps.googleapis.com/maps/api/place/textsearch/xml?query=things+to+do+#{input_location_for_activities}&key=#{mykey}"
 		format :json
 		response = HTTParty.get(url)
-		p '*' * 50
 		body = response['PlaceSearchResponse']['result']
 		p body
 		# need to iterate through the result hash to get the different activity places
@@ -34,27 +25,13 @@ class GooglePlacesApiAdapter
 		url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{location}&key=#{mykey}"
 		format :json
 		response = HTTParty.get(url)
-		# p self
-		# p '*' * 50
-		# p response.parsed_response['results'][0]['formatted_address']
 		body = response.parsed_response
-		# # p '*' * 50
 		location_name = body['results'][0]['formatted_address']
 		latitude = body['results'][0]['geometry']['location']['lat']
 		longitude = body['results'][0]['geometry']['location']['lng']
 		# p '*' * 50
 
-		# self.nearby_restaurants(latitude, longitude)
-		# p location_name
-		# p '*' * 50
-		# data = JSON.parse(response) #BECAUSE IT'S ALREADY PARSED
-		# p '^' * 50
-		# p data
-		# p '^' * 50
-		# be able to pass the latitude and longitude of location to nearby_restaurants
-		# p GooglePlacesApiAdapter.nearby_restaurants(latitude, longitude)
-		# p GooglePlacesApiAdapter.things_to_do(location)
-		p body
+		body
 	end
 
 	def self.get_latitude(location)
@@ -62,6 +39,7 @@ class GooglePlacesApiAdapter
 		url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{location}&key=#{mykey}"
 		format :json
 		response = HTTParty.get(url)
+		body = response.parsed_response
 		latitude = body['results'][0]['geometry']['location']['lat']
 	end
 
@@ -70,6 +48,7 @@ class GooglePlacesApiAdapter
 		url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{location}&key=#{mykey}"
 		format :json
 		response = HTTParty.get(url)
+		body = response.parsed_response
 		longitude = body['results'][0]['geometry']['location']['lng']
 	end
 
